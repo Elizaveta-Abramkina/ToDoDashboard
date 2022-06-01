bd761c909f6dfb691b0339754b542e6ebad3c5f1
Elizaveta Abramkina <elizaveta_abramkina@senla.eu>
2022-06-01 17:22:03 +0300

    Lines animations

--- src/components/Dashbourd/Dashboard.tsx
+++ src/components/Dashbourd/Dashboard.tsx
@@ -1,6 +1,6 @@
-import React, { useState} from 'react';
+import React, {useState} from 'react';
 import './dashbourd.scss'
-import { useDrop} from "react-dnd";
+import {useDrop} from "react-dnd";
 import DraggableBox from "../ DraggableBox/DraggableBox";
 import CalendarMini from '../calendarMini/CalendarMini';
 import {ItemTypes} from "./dragAndDrop/dragAndDrop";
@@ -19,18 +19,18 @@ interface DragItem {
 const Dashboard = () => {
 
     const [boxes, setBoxes] = useState<IBox>({
-        calendar: {top:0, left: 0},
+        calendar: {top: 0, left: 0},
         tasksList: {top: 300, left: 500}
     })
 
-    const moveBox = (id:string, left: number, top: number) =>{
-            setBoxes({...boxes, [id]:{top:top, left:left}})
-        }
+    const moveBox = (id: string, left: number, top: number) => {
+        setBoxes({...boxes, [id]: {top: top, left: left}})
+    }
 
     const [, drop] = useDrop(
-        ()=>({
+        () => ({
             accept: ItemTypes.BOX,
-            drop(item: DragItem, monitor ){
+            drop(item: DragItem, monitor) {
                 const delta = monitor.getDifferenceFromInitialOffset() as {
                     x: number
                     y: number
@@ -41,13 +41,15 @@ const Dashboard = () => {
                 moveBox(item.id, left, top)
                 return undefined
             },
-        }),[moveBox]
+        }), [moveBox]
     )
 
     return (
             <div ref={drop} className="layout">
-                <DraggableBox id='tasksList' left={boxes.tasksList.left} top={boxes.tasksList.top}> <TasksList/></DraggableBox>
-                <DraggableBox id='calendar' left={boxes.calendar.left} top={boxes.calendar.top}><CalendarMini/></DraggableBox>
+                <DraggableBox id='tasksList' left={boxes.tasksList.left} top={boxes.tasksList.top}>
+                    <TasksList/></DraggableBox>
+                <DraggableBox id='calendar' left={boxes.calendar.left}
+                              top={boxes.calendar.top}><CalendarMini/></DraggableBox>
             </div>
     );
 };
